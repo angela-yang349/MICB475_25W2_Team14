62c8704165de362fd2094bd4459dadf8c0f199e2
Angela Yang <angela.a.yang349@gmail.com>
2026-02-07 12:04:56 -0800

    making manifest and metadata tsv files

--- LabNotebook/R/Cleaning_MS_Data.R
+++ LabNotebook/R/Cleaning_MS_Data.R
@@ -3,5 +3,20 @@ library(tidyverse)
 meta <- read_delim(file = "ms_metadata.tsv", delim = "\t")
 manifest <- read_delim(file = "ms_manifest.tsv", delim = "\t")
 
+#filter out samples for RRMS
 filtered_meta <- subset(meta, disease_course != "RRMS")
+
+#filter out the same samples from meta in the manifest file
 filtered_manifest <- subset(manifest, `sample-id` %in% filtered_meta$`sample-id`)
+
+#save filtered files
+write.table(filtered_meta,
+            "ms_metadata_no_rrms.tsv",
+            sep = "\t", 
+            quote = FALSE, 
+            row.names = FALSE)
+write.table(filtered_manifest, 
+            "ms_manifest_no_rrms.tsv", 
+            sep = "\t", 
+            quote = FALSE, 
+            row.names = FALSE)
