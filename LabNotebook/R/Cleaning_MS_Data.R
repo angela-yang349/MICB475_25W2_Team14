cf4880cf1d5cbe569b0590a69a6b9d8eff44abd3
Angela Yang <angela.a.yang349@gmail.com>
2026-02-07 11:47:16 -0800

    filtering manifest

--- LabNotebook/R/Cleaning_MS_Data.R
+++ LabNotebook/R/Cleaning_MS_Data.R
@@ -1,4 +1,7 @@
 library(tidyverse)
 
 meta <- read_delim(file = "ms_metadata.tsv", delim = "\t")
+manifest <- read_delim(file = "ms_manifest.tsv", delim = "\t")
+
 filtered_meta <- subset(meta, disease_course != "RRMS")
+filtered_manifest <- subset(manifest, `sample-id` %in% filtered_meta$`sample-id`)
