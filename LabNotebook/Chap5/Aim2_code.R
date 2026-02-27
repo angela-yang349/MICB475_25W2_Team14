001ad7957a610d461cc8634459370ca1411b4060
Sadaf-H <hamidiayisha@gmail.com>
2026-02-26 22:34:28 -0800

    Alpha diversity changes to add stat test and figure updates

--- LabNotebook/Chap5/Aim2_code.R
+++ LabNotebook/Chap5/Aim2_code.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(phyloseq)
 library(vegan)
+library(ggsignif)
 
 #load rareified data
 load("LabNotebook/Chap2/ms_rare.RData")
@@ -11,11 +12,29 @@ PMS_treatments_richness_plot <-plot_richness(ms_rare, x = "treatments", measures
   xlab("Treatments") +
   ylab("Shannon Diversity Index") +
   geom_boxplot() +
-  ggtitle("Shannon Diversity of PMS Patients per Treatment and Healthy Controls")
+  geom_point() +
+  ggtitle("Shannon Diversity of PMS Patients per Treatment and Healthy Controls")+
+  theme_classic()
 
 PMS_treatments_richness_plot
 
 #save the file as a .png on your local computer
-ggsave(filename = "LabNotebook/Chap3/PMS_treatments_richness_plot.png",
-       PMS_treatments_richness_plot,
-       height=4, width=7)
+#ggsave(filename = "LabNotebook/Chap5/PMS_treatments_richness_plot.png",
+       #PMS_treatments_richness_plot,
+       #height=4, width=7)
+
+### Statistical test - Kruskal-Wallis rank sum test
+
+#first extract info
+Aim2_richness_estimate <- estimate_richness(ms_rare, 
+                                            measures = c("Observed", "Chao1", "ACE", "Shannon", "Simpson", "InvSimpson"))
+Aim2_alpha_samp_dat <- sample_data(ms_rare)
+Aim2_alpha_samp_and_richness <- data.frame(Aim2_alpha_samp_dat, Aim2_richness_estimate)
+
+view(Aim2_alpha_samp_and_richness)
+
+#run Kruskal-Wallis rank sum test
+kruskal_treatments <- kruskal.test( Shannon ~ treatments, data = Aim2_alpha_samp_and_richness)
+kruskal_treatments
+
+#none of these differences are significant since p=0.18 (not less than 0.05), thus further analysis is not needed
