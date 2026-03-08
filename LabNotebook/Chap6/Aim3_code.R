14d0194d891c9e70c19392bb8057632428347ee2
lilyou5 <lilyou5@student.ubc.ca>
2026-03-07 21:14:03 -0800

    Updated venn diagram with GA

--- LabNotebook/Chap6/Aim3_code.R
+++ LabNotebook/Chap6/Aim3_code.R
@@ -17,6 +17,7 @@ ms_treated_fingo <- subset_samples(phyloseq_RA, treatments == "Fingolimod")
 ms_treated_inter <- subset_samples(phyloseq_RA, treatments == "Interferon")
 ms_treated_oc <- subset_samples(phyloseq_RA, treatments == "ocrevus(rituxan)")
 ms_treated_dmf <- subset_samples(phyloseq_RA, treatments == "Dimethyl fumarate")
+ms_treated_ga <- subset_samples(phyloseq_RA, treatments == "Glatiramer acetate")
 
 # setting detection and prevalence thresholds for specific treatments venn diagram
 ms_control_ASVs <- core_members(ms_control, detection=0.001, prevalence = 0.5)
@@ -25,6 +26,7 @@ ms_treated_fingo_ASVs <- core_members(ms_treated_fingo, detection=0.001, prevale
 ms_treated_inter_ASVs <- core_members(ms_treated_inter, detection=0.001, prevalence = 0.5)
 ms_treated_oc_ASVs <- core_members(ms_treated_oc, detection=0.001, prevalence = 0.5)
 ms_treated_dmf_ASVs <- core_members(ms_treated_dmf, detection=0.001, prevalence = 0.5)
+ms_treated_ga_ASVs <- core_members(ms_treated_ga, detection=0.001, prevalence = 0.5)
 
 # subset data for general venn diagram 2
 ms_control_2 <- subset_samples(phyloseq_RA, treatment_status == "Control")
@@ -37,7 +39,7 @@ ms_untreated_2_ASVs <- core_members(ms_untreated_2, detection=0.001, prevalence 
 ms_treated_2_ASVs <- core_members(ms_treated_2, detection=0.001, prevalence = 0.5)
 
 # venn diagrams
-ms_venn_specific_treatments <- ggVennDiagram(x = list(Control = ms_control_ASVs, Untreated = ms_untreated_ASVs, Fingolimod = ms_treated_fingo_ASVs, Interferon = ms_treated_inter_ASVs, Ocrevus_rituxan = ms_treated_oc_ASVs, DMF = ms_treated_dmf_ASVs ))
+ms_venn_specific_treatments <- ggVennDiagram(x = list(Control = ms_control_ASVs, Untreated = ms_untreated_ASVs, Fingolimod = ms_treated_fingo_ASVs, Interferon = ms_treated_inter_ASVs, "Ocrevus (rituxan)" = ms_treated_oc_ASVs, DMF = ms_treated_dmf_ASVs, GA = ms_treated_ga_ASVs ))
 ms_venn_general <-ggVennDiagram(x =list(Control = ms_control_2_ASVs, Untreated = ms_untreated_2_ASVs, Treated=ms_treated_2_ASVs)) 
 
 # save venn diagram
