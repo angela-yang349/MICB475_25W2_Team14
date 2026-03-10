5cde2843735cebf16ca6ac39ec5df4245786d762
Trinity Ma <trinity.ma@kidney.ca>
2026-03-09 22:09:40 -0700

    Added code for aim 3b - indicator species analysis

---
+++ LabNotebook/Chap6/Aim3b_code.R
@@ -0,0 +1,30 @@
+# install package
+install.packages("indicspecies")
+
+# !/usr/bin/env Rscript
+library(tidyverse)
+library(phyloseq)
+library(indicspecies)
+
+# load data
+load("ms_rare.RData")
+ms_rare_no_RRMS_ctrl <- subset_samples(ms_rare, disease_course_control != "Control_RRMS")
+
+# Indicator Species/Taxa Analysis
+# glom to genus
+ms_genus <- tax_glom(ms_rare_no_RRMS_ctrl, "Genus", NArm = FALSE)
+ms_genus_RA <- transform_sample_counts(ms_genus, fun=function(x) x/sum(x))
+
+# ISA
+isa_ms <- multipatt(t(otu_table(ms_genus_RA)), cluster = sample_data(ms_genus_RA)$`donor_status`)
+summary(isa_ms)
+taxtable <- tax_table(ms_final) %>% as.data.frame() %>% rownames_to_column(var="ASV")
+
+# consider that your table is only going to be resolved up to the genus level, be wary of anything beyond the glomed taxa level
+res <- isa_s$sign %>% 
+  rownames_to_column(var="ASV") %>% 
+  left_join(taxtable) %>%
+  filter(p.value<0.05)
+
+# view results
+view(res)
