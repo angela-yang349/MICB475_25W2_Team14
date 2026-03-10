1aab3ccf68e370504736775ad7646ae2a9ad0777
Trinity Ma <trinity.ma@kidney.ca>
2026-03-09 22:28:01 -0700

    Updated code for aim 3b

--- LabNotebook/Chap6/Aim3b_code.R
+++ LabNotebook/Chap6/Aim3b_code.R
@@ -16,12 +16,12 @@ ms_genus <- tax_glom(ms_rare_no_RRMS_ctrl, "Genus", NArm = FALSE)
 ms_genus_RA <- transform_sample_counts(ms_genus, fun=function(x) x/sum(x))
 
 # ISA
-isa_ms <- multipatt(t(otu_table(ms_genus_RA)), cluster = sample_data(ms_genus_RA)$`donor_status`)
+isa_ms <- multipatt(t(otu_table(ms_genus_RA)), cluster = sample_data(ms_genus_RA)$`treatments`)
 summary(isa_ms)
-taxtable <- tax_table(ms_final) %>% as.data.frame() %>% rownames_to_column(var="ASV")
+taxtable <- tax_table(ms_rare_no_RRMS_ctrl) %>% as.data.frame() %>% rownames_to_column(var="ASV")
 
 # consider that your table is only going to be resolved up to the genus level, be wary of anything beyond the glomed taxa level
-res <- isa_s$sign %>% 
+res <- isa_ms$sign %>% 
   rownames_to_column(var="ASV") %>% 
   left_join(taxtable) %>%
   filter(p.value<0.05)
