2f631220369de46beccf592b8eaa93ddf5fab60f
trinityma <147008103+trinityma@users.noreply.github.com>
2026-03-10 13:06:27 -0700

    Update Aim3b_code.R

--- LabNotebook/Chap6/Aim3b_code.R
+++ LabNotebook/Chap6/Aim3b_code.R
@@ -34,3 +34,20 @@ view(res)
 
 #save table to github
 write.csv(res, "ISA_results.csv", row.names = FALSE)
+
+# ISA for grouped treatments
+isa_ms <- multipatt(t(otu_table(ms_genus_RA)), cluster = sample_data(ms_genus_RA)$`treatment_status`)
+summary(isa_ms)
+taxtable <- tax_table(ms_rare_no_RRMS_ctrl) %>% as.data.frame() %>% rownames_to_column(var="ASV")
+
+# consider that your table is only going to be resolved up to the genus level, be wary of anything beyond the glomed taxa level
+res <- isa_ms$sign %>% 
+  rownames_to_column(var="ASV") %>% 
+  left_join(taxtable) %>%
+  filter(p.value<0.05)
+
+# view results
+view(res)
+
+#save table to github
+write.csv(res, "ISA_grouped_results.csv", row.names = FALSE)                                       
